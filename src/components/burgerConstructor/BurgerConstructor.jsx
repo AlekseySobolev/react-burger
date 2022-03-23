fa45ec01e14cc1d23c0fdf954b2d7098788d45ea
Aleksey Sobolev <Sobzone@yandex.ru>
2022-03-23 15:02:52 +0300

    1.1

--- src/components/burgerConstructor/BurgerConstructor.jsx
+++ src/components/burgerConstructor/BurgerConstructor.jsx
@@ -8,73 +8,121 @@ import burgerConstructorStyles from './burgerConstructor.module.css';
 const burgerElements = [
     {
         "name": "Краторная булка N-200i",
-        "type": "bun",     
-        "image":"https://code.s3.yandex.net/react/code/bun-02.png"
+        "type": "bun",
+        "image": "https://code.s3.yandex.net/react/code/bun-02.png"
 
     },
     {
-        "name":"Соус традиционный галактический",
-        "type":"sauce",
-        "image":"https://code.s3.yandex.net/react/code/sauce-03.png"
+        "name": "Соус традиционный галактический",
+        "type": "sauce",
+        "image": "https://code.s3.yandex.net/react/code/sauce-03.png"
     },
     {
-        "name":"Мясо бессмертных моллюсков Protostomia",
-        "type":"main",
-        "image":"https://code.s3.yandex.net/react/code/meat-02.png"
+        "name": "Мясо бессмертных моллюсков Protostomia",
+        "type": "main",
+        "image": "https://code.s3.yandex.net/react/code/meat-02.png"
     },
     {
-        "name":"Плоды Фалленианского дерева",
-        "type":"main",
-        "image":"https://code.s3.yandex.net/react/code/sp_1.png"
+        "name": "Плоды Фалленианского дерева",
+        "type": "main",
+        "image": "https://code.s3.yandex.net/react/code/sp_1.png"
 
     },
     {
-        "name":"Хрустящие минеральные кольца",
-        "type":"main",
-        "image":"https://code.s3.yandex.net/react/code/mineral_rings.png"
+        "name": "Хрустящие минеральные кольца",
+        "type": "main",
+        "image": "https://code.s3.yandex.net/react/code/mineral_rings.png"
     },
     {
-        "name":"Хрустящие минеральные кольца",
-        "type":"main",
-        "image":"https://code.s3.yandex.net/react/code/mineral_rings.png"
+        "name": "Хрустящие минеральные кольца",
+        "type": "main",
+        "image": "https://code.s3.yandex.net/react/code/mineral_rings.png"
+    },
+    ,
+    {
+        "name": "Хрустящие минеральные кольца",
+        "type": "main",
+        "image": "https://code.s3.yandex.net/react/code/mineral_rings.png"
+    },
+    ,
+    {
+        "name": "Хрустящие минеральные кольца",
+        "type": "main",
+        "image": "https://code.s3.yandex.net/react/code/mineral_rings.png"
     },
     {
         "name": "Краторная булка N-200i",
-        "type": "bun",     
-        "image":"https://code.s3.yandex.net/react/code/bun-02.png"
+        "type": "bun",
+        "image": "https://code.s3.yandex.net/react/code/bun-02.png"
     }
 ]
 
-const renderConstructorElement = (e, index, array) => {
-    let typeValue = "";
-    if(index === 0){
-        typeValue = "top";
-    }
-    else if (index === (array.length -1)){
-        typeValue = "bottom";
-    }
-    return( 
-    <li key = {index} className = {burgerConstructorStyles.listElement}>
-        <DragIcon type="primary" />
-        <ConstructorElement type = {typeValue} isLocked = {e.type === "bun"} text={e.name} price={200}  thumbnail={e.image}/>
-    </li>    
-   )
+const renderTopConstructorElement = (arrElem) => {
+  
+    return (
+        <>
+ 
+                <li key={arrElem.index} className={burgerConstructorStyles.listElement + " ml-8 mr-2"}>
+                    <ConstructorElement type={"top"} isLocked={true} text={arrElem.e.name + " (верх)"} price={200} thumbnail={arrElem.e.image} />
+                </li>
+    
+        </>
+    )
+};
+
+const renderBottomConstructorElement = (arrElem) => {
+    
+    return (
+        <>
+         
+                <li key={arrElem.index} className={burgerConstructorStyles.listElement  + " ml-8 mr-2"}>
+                    <ConstructorElement type={"bottom"} isLocked={true} text={arrElem.e.name + " (низ)"} price={200} thumbnail={arrElem.e.image} />
+                </li>
+            
+        </>
+    )
+};
+const renderMiddleConstructorElement = (e, index, array) => {
+
+    let isMiddle = (index === 0) || (index === (array.length - 1)) ? false : true;
+
+    // if ((index === 0) || (index === (array.length - 1))) {
+    //     isMiddle = false;
+    // }
+  
+    return (
+        <>
+            {isMiddle &&
+                <li key={index} className={burgerConstructorStyles.middleListElement + " mr-2"}>
+                    <DragIcon type="primary" />
+                    <ConstructorElement isLocked={!isMiddle} text={e.name} price={200} thumbnail={e.image} />
+                </li>
+            }
+        </>
+    )
 };
 
 function BurgerConstructor(props) {
 
-        return(
-            
-            <section className = {burgerConstructorStyles.section}>
-                <ul className ={burgerConstructorStyles.list + " mt-5 ml-4 mb-10 mr-2"}> 
-                  {burgerElements.map(renderConstructorElement)}
-                </ul> 
-                <OrderBox onClick = {props.onCloseClick}/>
-             </section>
-            
+    let firstElement = {e: burgerElements[0], index: 0};
+    let lastElement = {e: burgerElements[burgerElements.length -1], index: [burgerElements.length -1]};
 
-        );
-    
+    return (
+
+        <section className={burgerConstructorStyles.section}>
+            <ul className={burgerConstructorStyles.list + " mt-15 ml-4 mb-10"}>
+                {renderTopConstructorElement(firstElement)}
+                <div className={burgerConstructorStyles.list + " " + burgerConstructorStyles.constructorBox}>
+                    {burgerElements.map(renderMiddleConstructorElement)}
+                </div>
+                {renderBottomConstructorElement(lastElement)}
+            </ul>
+            <OrderBox onClick={props.onClick} />
+        </section>
+
+
+    );
+
 }
 
 export default BurgerConstructor;
