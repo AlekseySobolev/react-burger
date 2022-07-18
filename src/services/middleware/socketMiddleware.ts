f23263de34e0172b6e5af4555b36f2816491ed31
Aleksey Sobolev <Sobzone@yandex.ru>
2022-07-19 02:13:15 +0300

    month-10/step-1 4.0

--- src/services/middleware/socketMiddleware.ts
+++ src/services/middleware/socketMiddleware.ts
@@ -2,7 +2,7 @@ import { MiddlewareAPI } from 'redux';
 import { IWsActions } from '../../services/store'
 import { AppDispatch, RootState } from '../types';
 
-export const socketMiddleware = (wsActions: IWsActions) => (store) => {
+export const socketMiddleware = (wsActions: IWsActions) => (store: MiddlewareAPI<any, RootState>) => {// если в первый параметр MiddlewareAPI передаю AppDispatch появляется ошибка
 
       let socket: WebSocket | null = null;
   
